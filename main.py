0bbb78be4dd500e9f12f86f8318189037b123089
hvhi56 <3011760@GMAIL.COM>
2025-07-24 14:38:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -123,8 +123,7 @@ def text_to_mp3(text, filename='output.mp3'):
 
     audio_config = texttospeech.AudioConfig(
         audio_encoding=texttospeech.AudioEncoding.MP3,
-        speaking_rate=1.3,
-        pitch=2.0
+        speaking_rate=1.3
     )
 
     response = client.synthesize_speech(
